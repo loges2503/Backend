d25501dbfba6c0753e78db0940bb37ec46caddc1
Logs01 <99220040298@klu.ac.in>
2026-09-10 10:00:22 +0530

    new files

---
+++ Assignment/Day_2.js
@@ -0,0 +1,53 @@
+// TASK TWO
+
+console.log(" TASK ONE OUTPUT");    
+let a= "";
+for (let a=1;a <=10;a++){
+    console.log(a);
+}
+
+// TASK TWO 
+console.log("TASK TWO OUTPUT");  
+let b = "";
+for(let b=1;b<=20;b++){
+    if(b % 2 == 0){
+        console.log(b);
+        
+    }
+}
+
+console.log("TASK THREE OUTPUT");  
+// Task Three 
+let num =5;
+
+for(let c =1;c<=10;c++){
+    console.log(`5 x ${c} = ${5 * c}`);
+}
+
+
+console.log("TASK FOUR OUTPUT"); 
+// Task Four
+
+for(let d = 1;d<=6;d++){
+    let four="";
+    for( let e = 1;e<d;e++){
+        four = four + " *";
+        
+        
+    }
+    console.log(four);
+}
+
+
+console.log(" TASK FIVE OUTPUT"); 
+// Task Five
+
+for(let f = 1;f<=6;f++){
+    let five="";
+    for(let e = 1;e<f;e++){
+        five = five + e+" " ;
+        
+        
+    }
+    console.log(five);
+}
